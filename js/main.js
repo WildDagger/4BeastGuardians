4c241af281f8dc49c9fe48e6c6d6d70c02650a91
WildDagger Ho <hoyunxian@gmail.com>
2021-12-04 12:23:17 +0800

    20211204 新增模組

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ window.addEventListener('DOMContentLoaded', function() {
     const navbar = document.querySelectorAll('.navbar')
     //const navbarcontent = document.querySelector('.navbar .navbar-content')
     //const navbarbutton = document.querySelector('.navbar .navbar-button')
+    const cards = document.querySelectorAll('.section .inner .contents .card')
+    const modal = document.getElementById('modal')
+    const modalIcon = modal.querySelector('.modal-icon-inner')
+    const modalHeader = modal.querySelector('.modal-header')
+    const modalContent = modal.querySelector('.modal-content')
 
     navbar.forEach(function(element) {
         var navbarContent = element.querySelectorAll('.navbar-content')
@@ -34,6 +39,35 @@ window.addEventListener('DOMContentLoaded', function() {
         }, 1000 / 15))
 
         window.dispatchEvent(new Event('scroll'))
+    })
+
+    cards.forEach(function(card) {
+        card.addEventListener('click', function(e) {
+            var icon = card.querySelector('.card-background-inner')
+            var titleNodes = card.querySelector('.card-title').childNodes
+            var contentNodes = card.querySelector('.card-content').childNodes
+
+            modalIcon.style.backgroundImage = icon.style.backgroundImage
+
+            modalHeader.innerHTML = ''
+            modalContent.innerHTML = ''
+
+            titleNodes.forEach(function(node) {
+                var cln = node.cloneNode(true)
+                modalHeader.appendChild(cln)
+            })
+            contentNodes.forEach(function(node) {
+                var cln = node.cloneNode(true)
+                modalContent.appendChild(cln)
+            })
+            
+
+            modal.classList.add('open')
+        })
+    })
+
+    modal.addEventListener('click', function(e) {
+        modal.classList.remove('open')
     })
 
     /*navbarbutton.addEventListener('on', function(e) {
